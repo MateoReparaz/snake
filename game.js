77ad8a86783e4d6d096bd9d340b8c6c86d7b37df
MateoReparaz <mateoreparaz1989@gmail.com>
2018-06-27 14:03:26 +0200

    Colisiones con self conseguidas, estamos con el score, faltarán niveles y otras frutas

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 function Game() {
   this.canvas = document.getElementById("canvas");
   this.ctx = this.canvas.getContext("2d");
-  this.fps = 120;
+  this.fps = 60;
   this.reset();
   this.score = 0;
-  this.audioEat = new Audio('audio/eat.mp3');
-  this.audioDead = new Audio('audio/dead.mp3')
+  this.audioEat = new Audio("audio/eat.mp3");
+  this.audioDead = new Audio("audio/dead.mp3");
 }
 
 Game.prototype.start = function() {
@@ -14,7 +14,8 @@ Game.prototype.start = function() {
       this.clear();
       this.draw();
       this.move();
-      this.colision();
+      this.colisionLimits();
+      this.colisionSelf();
       this.snake.grow();
     }.bind(this),
     this.fps
@@ -42,19 +43,25 @@ Game.prototype.move = function() {
   this.snake.move();
 };
 
-Game.prototype.colision = function() {
+Game.prototype.colisionLimits = function() {
   this.eat();
   if (
     this.snake.cobra.x[0] < 0 ||
     this.snake.cobra.x[0] > 768 ||
     this.snake.cobra.y[0] < 0 ||
     this.snake.cobra.y[0] > 768
-    
   ) {
-    this.audioDead.play();
-    alert("Try Again")
-    location.reload();
+    this.gameOver();
   }
+};
+
+Game.prototype.colisionSelf = function() {
+  for (let i = 1; i < this.snake.cobra.length; i++)
+    if (
+      this.snake.cobra.x[0] == this.snake.cobra.x[i] &&
+      this.snake.cobra.y[0] == this.snake.cobra.y[i]
+    )
+      this.gameOver();
 };
 
 Game.prototype.eat = function() {
@@ -71,6 +78,14 @@ Game.prototype.eat = function() {
 };
 
 Game.prototype.updateScore = function() {
+  document.getElementById("score").innerText = "SCORE:" + this.score
   this.score += 7;
   this.snake.cobra.length++;
+};
+
+Game.prototype.gameOver = function() {
+  this.audioDead.play();
+  alert("Try Again");
+  location.reload();
+  console.log(this.snake.cobra.y, this.snake.cobra.x);
 };
